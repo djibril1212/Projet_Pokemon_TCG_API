d5d424da3473d7c398b62a6e50fcfe33402af6a2
djibril1212 <95239684+djibril1212@users.noreply.github.com>
2025-01-30 11:00:37 +0100

    add method to CarteRepository for finding cards by Dresseur

--- src/main/java/fr/efrei/pokemon_tcg/repositories/CarteRepository.java
+++ src/main/java/fr/efrei/pokemon_tcg/repositories/CarteRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CarteRepository extends JpaRepository<Carte, String> {
+
     List<Carte> findByDresseur(Dresseur dresseur);
 }
