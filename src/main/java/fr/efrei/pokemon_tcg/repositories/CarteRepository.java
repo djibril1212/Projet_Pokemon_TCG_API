55df1bff9f2f606c32f7a01fd9254376a6aa3c9e
djibril1212 <95239684+djibril1212@users.noreply.github.com>
2025-01-30 11:00:07 +0100

    refactor DrawController to return ResponseEntity for card drawing response

---
+++ src/main/java/fr/efrei/pokemon_tcg/repositories/CarteRepository.java
@@ -0,0 +1,11 @@
+package fr.efrei.pokemon_tcg.repositories;
+
+import fr.efrei.pokemon_tcg.models.Carte;
+import fr.efrei.pokemon_tcg.models.Dresseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CarteRepository extends JpaRepository<Carte, String> {
+    List<Carte> findByDresseur(Dresseur dresseur);
+}
