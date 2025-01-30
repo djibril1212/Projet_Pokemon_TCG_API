757e281744748d16a9dfd490bb38ab65a05d2d61
djibril1212 <95239684+djibril1212@users.noreply.github.com>
2025-01-30 10:17:34 +0100

    add attack and rarity attributes to Pokemon model

--- src/main/java/fr/efrei/pokemon_tcg/models/Pokemon.java
+++ src/main/java/fr/efrei/pokemon_tcg/models/Pokemon.java
@@ -11,11 +11,15 @@ public class Pokemon {
 	private String uuid;
 
 	private String nom;
-
 	private Integer niveau;
 
 	@Enumerated(EnumType.STRING)
 	private TypePokemon type;
+
+	private String attaque1;
+	private String attaque2;
+
+	private int rarete; // 1 à 5 étoiles
 
 	public String getNom() {
 		return nom;
@@ -39,6 +43,30 @@ public class Pokemon {
 
 	public void setType(TypePokemon type) {
 		this.type = type;
+	}
+
+	public String getAttaque1() {
+		return attaque1;
+	}
+
+	public void setAttaque1(String attaque1) {
+		this.attaque1 = attaque1;
+	}
+
+	public String getAttaque2() {
+		return attaque2;
+	}
+
+	public void setAttaque2(String attaque2) {
+		this.attaque2 = attaque2;
+	}
+
+	public int getRarete() {
+		return rarete;
+	}
+
+	public void setRarete(int rarete) {
+		this.rarete = rarete;
 	}
 
 	public String getUuid() {
